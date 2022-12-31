84d855e16cb57c19b92c33b15e184c8ae97b4c2a
silverace_71 <alekshilderbrand@gmail.com>
2022-12-30 19:22:14 -0700

    added cron

--- cf-setup.sh
+++ cf-setup.sh
@@ -6,12 +6,24 @@ echo "(2)|fedora"
 echo "(3)|arch"
 read os
 if [[ $os == "1" ]]; then
+echo "do you have crontab installed(y/n)?"
+read indeed
+if [[ $indeed == "y"]]; then
+sleep 1
+else
+sudo apt install cron -y
+fi
 wget https://raw.githubusercontent.com/silverace71/ez-cf-tunnel/main/cf-deb-amd64.sh
 sudo chmod +x cf-deb-amd64.sh
 ./cf-deb-amd64.sh
 else
     if [[ $os == "2" ]]; then
-    echo "fedora"
+    echo "do you have crontab installed(y/n)?"
+    read indeed
+    if [[ $indeed == "y"]]; then
+    sleep 1
+    else
+
     else
     echo "arch"
     fi
