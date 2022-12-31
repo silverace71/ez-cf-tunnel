44450ae19feb8bd48568ad3cd81cb265777f424f
silverace_71 <alekshilderbrand@gmail.com>
2022-12-30 18:10:31 -0700

    made this work for testing

--- cf-setup.sh
+++ cf-setup.sh
@@ -6,7 +6,7 @@ echo "(2)|fedora"
 echo "(3)|arch"
 read os
 if [[ $os == "1" ]]; then
-echo "debian"
+wget https://raw.githubusercontent.com/silverace71/ez-cf-tunnel/main/cf-deb-amd64.sh
 else
     if [[ $os == "2" ]]; then
     echo "fedora"
